9279c58058177068022371f36b0e11ca9a195222
Cason Clagg <cason@cason.cc>
2014-10-21 20:33:08 -0500

    Fixed a bug

--- src/ios/CDVRealtimeCamera.m
+++ src/ios/CDVRealtimeCamera.m
@@ -183,11 +183,11 @@
 
 
 	// just send an empty event telling the app to pull the buffer
-	NSArray* params = [NSArray arrayWithObjects: [NSNumber numberWithInteger:width], [NSNumber numberWithInteger:height], nil];
+	NSArray* params = [NSArray arrayWithObjects: [NSNumber numberWithInteger:bufferWidth], [NSNumber numberWithInteger:bufferHeight], nil];
 	CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
 #else
 	// call the result callback with the image data as a buffer
-	NSArray* params = [NSArray arrayWithObjects: [NSNumber numberWithInteger:width], [NSNumber numberWithInteger:height], brightness, nil];
+	NSArray* params = [NSArray arrayWithObjects: [NSNumber numberWithInteger:bufferWidth], [NSNumber numberWithInteger:bufferHeight], brightness, nil];
 	CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsMultipart: params ];
 #endif
 	[pluginResult setKeepCallbackAsBool:YES]; // keep the callback so we can use it again
